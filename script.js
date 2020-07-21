34be2ecc0c2d8bfefd1bb556ec8cc2782f4fcd4b
Stephanie Landgraf <stlandgraf1234@gmail.com>
2020-07-20 21:52:01 -0500

    fixed layout

--- script.js
+++ script.js
@@ -1,60 +1,14 @@
-$("document").ready(function () {
-    $("img").addClass("animated fadeInDown");
-    $("h1").addClass("animated fadeInDown");
-    $("h2").addClass("animated fadeInDown");
-    $("h3").addClass("animated fadeInDown");
-    $("h4").addClass("animated fadeInDown");
-    $("h5").addClass("animated fadeInDown");
-    $("h6").addClass("animated fadeInDown");
-    $("p").addClass("animated fadeInDown");
-    $("hr").addClass("animated fadeInDown");
-    $("i").addClass("animated fadeInDown");
-    $("section").addClass("animated fadeInDown");
-    $("article").addClass("animated fadeInDown");
-    $("ul").addClass("animated fadeInDown");
-    
-    $.scrollIt();
-  });
+var slideIndex = 0;
+showSlides();
 
-  paper.install(window);
-  paper.setup(document.getElementById("canvas"));
-  /* ====================== *
-   *                        *
-   * ====================== */
-  var wave;
-  var segmentAmount = 5;
-  var waveHeight = 15;
-  
-  initiateWave();
-  
-  function initiateWave() {
-    wave = new Path({
-      fillColor: 'black',
-      strokeColor: 'black',
-      strokeWidth: 5,
-      closed: true,
-    });
-    for (var i = 0; i <= segmentAmount; i++) {
-      wave.add(new Point((i / segmentAmount) * view.size.width, view.size.height));
-    }
-    // Complete Shape
-    wave.add(new Point(view.size.width, view.size.height));
-    wave.add(new Point(0, view.size.height));
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
   }
-  
-  /* ====================== *
-   * Animation              *
-   * ====================== */
-  view.onFrame = function(event) {
-      for (var i = 0; i <= segmentAmount; i++) {
-          var segment = wave.segments[i];
-          var sinus = Math.sin(event.time * 3 + i);
-          segment.point.y = sinus * waveHeight + 25;
-      }
-      wave.smooth();
-  }
-  
-  view.onResize = function() {
-    wave.remove();
-    initiateWave();
-  };
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1} 
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 4000); // Change image every 12 seconds
+}
